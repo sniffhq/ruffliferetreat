6fdee07c9808bde4407a98727f96b04ef24dc623
Charles Brown <charles.brown@sniffhq.com>
2026-08-21 12:24:27 -0400

    Remove blank migration file blocking flask db upgrade

--- migrations/versions/a1b2c3d4e5f6_daycare_attendance_waived.py
+++
@@ -1,4 +0,0 @@
-# This file is intentionally left blank.
-# The actual migration was renamed to dc3f8a921b04_daycare_attendance_waived.py
-# to avoid a revision ID collision with a1b2c3d4e5f6_add_sms_opt_in.py
-# and a1b2c3d4e5f6_boarding_booking_number.py.
